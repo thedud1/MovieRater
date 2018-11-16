b7209a5c0e4a669783804eaf05fc0c7289991cf7
Jun <173331c@mymail.nyp.edu.sg>
2018-11-16 22:18:20 +0800

    added onclick visible

--- app/src/main/java/com/nyp/sit/dit/it2107/movierater/MainActivity.kt
+++ app/src/main/java/com/nyp/sit/dit/it2107/movierater/MainActivity.kt
@@ -2,11 +2,22 @@ package com.nyp.sit.dit.it2107.movierater
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nc16.setOnClickListener{
+            if (nc16.isChecked)
+            { violence.visibility=View.VISIBLE
+                languageUsed.visibility=View.VISIBLE
+            }
+
+        }
     }
 }
